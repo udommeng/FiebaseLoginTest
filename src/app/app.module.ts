5acfd7374fba92606b120b2869789e95a86d296b
udommeng <udommeng@gmail.com>
2022-04-04 12:11:33 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { AuthService } from './auth.service';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { SignInComponent } from './signin/signin.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SignInComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAxglaRB-MVKhR2ule1fArVfjHB52j10-c",
+    authDomain: "ez-school-pass.firebaseapp.com",
+    projectId: "ez-school-pass",
+    storageBucket: "ez-school-pass.appspot.com",
+    messagingSenderId: "106727084125",
+    appId: "1:106727084125:web:e261f3ebc5f01c8657bdb4",
+    measurementId: "G-X428HJ37RH"
+  }
 };
 
 /*
